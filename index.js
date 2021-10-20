6160c0de2e6bc49a56a438d4eb3b196aa244a10a
TSephanya Brun <tsbrun@icloud.com>
2021-10-20 00:59:30 -0400

    GET categories pseudocode

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("in index.js") // log to console in order to check connection
+
+// When some event happens, I want to make what kind of fetch and then manipulate the DOM in what way?
+
+// event: page loads
+// fetch: GET categories
+// DOM: list categories and append items to respective divs
