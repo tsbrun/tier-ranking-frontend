7cfed8b1783e402e254a741e2d573b9cbbcbc50c
TSephanya Brun <tsbrun@icloud.com>
2021-10-20 14:02:51 -0400

    Bug - async function did not resolve infinite fetch loop

--- index.js
+++ index.js
@@ -2,23 +2,30 @@ console.log("in index.js") // log to console in order to check connection
 
 // When some event happens, I want to make what kind of fetch and then manipulate the DOM in what way?
 
-// const BACKEND_URL = 'http://localhost:3000/api/v1'
+const BACKEND_URL = 'http://localhost:3000/api/v1'
 
 // event: page loads
 // fetch: GET categories
 // DOM: list categories and append items to respective divs
 
-document.addEventListener('DOMContentLoaded', () => {
-    // fetch(BACKEND_URL+'/categories')
+// document.addEventListener('DOMContentLoaded', () => {
+//     fetchCategories()
+//     .then(categories => console.log(categories))
+// })
+
+// fetch(BACKEND_URL+'/categories')
     // .then(response => response.json())
     // .then(categories => {
     //     console.log(categories)
     // })
-    fetchCategories()
-    .then(categories => console.log(categories))
-})
 
-function fetchCategories() {
-    return fetch(`${BACKEND_URL}/categories`)
-    .then(resp => resp.json())
+async function fetchCategories() {
+    // return fetch(`${BACKEND_URL}/categories`)
+    // .then(resp => resp.json())
+    const response = await fetch(`${BACKEND_URL}/categories`)
+
+    var data = await response.json()
+    console.log(data)
 }
+
+// fetchCategories()
