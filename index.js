e5ab5a49bb6b1dfd231d53472edd74cfef260e73
TSephanya Brun <tsbrun@icloud.com>
2021-10-20 01:09:38 -0400

    check if event listener fires after DOM content loads

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ console.log("in index.js") // log to console in order to check connection
 
 // When some event happens, I want to make what kind of fetch and then manipulate the DOM in what way?
 
+const BACKEND_URL = 'localhost:3000'
+
 // event: page loads
 // fetch: GET categories
 // DOM: list categories and append items to respective divs
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('loaded') // check if event listener is functional
+})
