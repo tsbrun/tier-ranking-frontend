feddaeb4ba74c24338d35aed72531c506ba4628f
TSephanya Brun <tsbrun@icloud.com>
2021-10-20 15:05:12 -0400

    Fetch categories and pseudocode

--- index.js
+++ index.js
@@ -9,9 +9,25 @@ console.log("in index.js") // log to console in order to check connection
 // DOM: list categories and append associated items underneath (w/ div for uncategorized)
 
 document.addEventListener('DOMContentLoaded', () => {
+    // fetch categories
+    // markup titles as headings 
+    // append divs underneath titles 
+    // div class = category.name
+    fetchCategories()
+    .then(categories => console.log(categories))
+
+    // fetch items
+    // markup items as images w/ names underneath
+    // append to div where class == item.category.name
+    // if category == false, append to div.uncategorized
     fetchItems()
     .then(items => console.log(items))
 })
+
+function fetchCategories() {
+    return fetch(`${api_url}/categories`)
+    .then(resp => resp.json())
+}
 
 function fetchItems() {
     return fetch(`${api_url}/items`)
