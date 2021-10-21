94b35e9fc3a09b9ebabb2987eefc869ab83a8d0a
TSephanya Brun <tsbrun@icloud.com>
2021-10-20 21:28:50 -0400

    Refactor renderCategory() function into Category class method

--- src/category.js
+++ src/category.js
@@ -4,4 +4,20 @@ class Category {
         this.title = title
         Category.all.push(this) // push all instances of Category into an array
     }
+
+    renderCategoryDiv() {
+        // title heading
+        let title = document.createElement('h2')
+        let text = document.createTextNode(this.title)
+        title.appendChild(text)
+
+        // div for associated items
+        let categoryDiv = document.createElement('div')
+        categoryDiv.classList.add(this.title)
+        categoryDiv.classList.add('category')
+
+        // append to div.main
+        document.querySelector('.main').appendChild(categoryDiv)
+        document.querySelector('.main').insertBefore(title, categoryDiv)
+    }
 }
