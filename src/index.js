8d0cb95adda5073e77bea6e83e10fd54f1535cc0
TSephanya Brun <tsbrun@icloud.com>
2021-10-23 00:09:13 -0400

    Function to select tier to drag item into

--- src/index.js
+++ src/index.js
@@ -77,6 +77,21 @@ function addItemsToTierRanking() {
     })
 }
 
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging')]
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect()
+        const offset = y - box.top - box.height / 2
+
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child }
+        } else {
+            return closest
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element
+}
+
 // fetch, create, and display functions
 
 function fetchCategories() {
